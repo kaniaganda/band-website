edf53aa8bf26de1d9e07a33a245b0af0a29b752a
Kania Ganda <kaniagandacs@gmail.com>
2021-07-21 00:19:04 -0700

    Added fetch POST for Stripe

--- public/store.js
+++ public/store.js
@@ -45,6 +45,18 @@ var stripeHandler = StripeCheckout.configure({
                 quantity: quantity
             })
         }
+
+        fetch("/purchase", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                items: items
+            })
+        })
     }
 })
 
