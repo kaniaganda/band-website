80f28bf057928a2720cb9980034b4289d7059b80
Kania Ganda <kaniagandacs@gmail.com>
2021-07-20 23:59:34 -0700

    Begin fetch POST for Stripe

--- public/store.js
+++ public/store.js
@@ -32,8 +32,19 @@ var stripeHandler = StripeCheckout.configure({
     key: stripePublicKey,
     locale: "auto",
     token: function(token) {
-        console.log(token)
-        
+        var items = [];
+        var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+        var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+        for (var i = 0; i < cartRows.length; i++) {
+            var cartRow = cartRows[i];
+            var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
+            var quantity = quantityElement.value;
+            var id = cartRow.dataset.itemId;
+            items.push({
+                id: id,
+                quantity: quantity
+            })
+        }
     }
 })
 
@@ -83,13 +94,15 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
     var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
     var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
-    addItemToCart(title, price, imageSrc);
+    var id = shopItem.dataset.itemId;
+    addItemToCart(title, price, imageSrc, id);
     updateCartTotal();
 }
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(title, price, imageSrc, id) {
     var cartRow = document.createElement("div");
     cartRow.classList.add("cart-row");
+    cartRow.dataset.id = id;
     var cartItems = document.getElementsByClassName("cart-items")[0];
     var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
     for (var i = 0; i < cartItemNames.length; i++) {
